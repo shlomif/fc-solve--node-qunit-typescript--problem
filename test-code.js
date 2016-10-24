09e33c32ce92b2ac3c9a7a6d76077dfc3700cbc8
Shlomi Fish <shlomif@shlomifish.org>
2016-10-24 20:53:06 +0300

    Add the test code.

---
+++ test-code.js
@@ -0,0 +1,19 @@
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+requirejs(['./dest/js/fcs-validate.js', ],
+    function   (fcs_valid) {
+        requirejs(['./dest/js/web-fc-solve-tests--fcs-validate.js'],
+            function (test_valid) {
+                test_valid.test_fcs_validate();
+                return;
+            });
+        return;
+    });
+
